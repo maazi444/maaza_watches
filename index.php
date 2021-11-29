b390fe88407d59bd555fb250516d962a8c5bbabf
maazi444 <95238151+maazi444@users.noreply.github.com>
2021-11-29 21:43:08 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,167 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<script src="https://kit.fontawesome.com/12583d8ffd.js" crossorigin="anonymous"></script>
+	<link rel="shortcut icon" type="image/jpg" href="img/webLogo.png"/>
+	<title>Maaz A.</title>
+</head>
+<body>
+	<section class="container">
+		<header>
+			<div class="brandLogo">
+				<a href="index.php"><img src="img/brandLogo.png"/></a>
+			</div>
+			<div class="menuLinks">
+				<ul>
+					<li><a href="#collection">Collection</a></li>
+					<li><a href="#about">About</a></li>
+					<li><a href="#contact">Contact Us</a></li>
+				</ul>
+			</div>
+		</header>
+		<div class="mainContent">
+			<ul>
+				<li><a href="#">Men's<br/><span>collection</span></a></li>
+				<li class="mainContentImg"><img src="img/watch4e.png"/></li>
+				<li><a href="#">Women's<br/><span>collection</span></a></li>
+			</ul>
+		</div>
+	</section>
+	<section class="collectionBrand" id="collection">
+		<div class="cbHeadings">
+			<h1 class="cbMainHeading">Our Watches are crafted with<br/>scrupulous attention to detail...</h1>
+			<p class="cbMainSubheading">
+				Explore the collection of prestigious, high-precision timepieces. We offers a wide assortment of Oyster Perpetual and Cellini watches to suit any wrist.<br/>Discover the broad selection of MaazA watches to find a perfect combination of style and functionality.
+			</p>
+		</div>
+		<div class="watchesCollection">
+			<div class="card">
+				<div class="imgSection1"></div>
+				<div class="titleSection">
+					<h1>DateJust 36</h1>
+					<p>New Model 2021 Oyster, 36 mm, Oystersteel</p>
+					<button class="cardviewBtn">View</button>
+				</div>
+			</div>
+			<div class="card">
+				<div class="imgSection2"></div>
+				<div class="titleSection">
+					<h1>Submariner</h1>
+					<p>Oyster, 41 mm, Oystersteel</p>
+					<button class="cardviewBtn">View</button>
+				</div>
+			</div>
+			<div class="card">
+				<div class="imgSection3"></div>
+				<div class="titleSection">
+					<h1>Yacht-Master II</h1>
+					<p>Oyster, 44 mm, Oystersteel and Everose gold</p>
+					<button class="cardviewBtn">View</button>
+				</div>
+			</div>
+			<div class="card">
+				<div class="imgSection4"></div>
+				<div class="titleSection">
+					<h1>Sea-Dweller</h1>
+					<p>Oyster, 43 mm, Oystersteel and yellow gold</p>
+					<button class="cardviewBtn">View</button>
+				</div>
+			</div>
+			<div class="card">
+				<div class="imgSection5"></div>
+				<div class="titleSection">
+					<h1>Day-Date 36</h1>
+					<p>Oyster, 36 mm, white gold and diamonds</p>
+					<button class="cardviewBtn">View</button>
+				</div>
+			</div>
+		</div>
+		<button class="viewMoreBtn">Explore</button>
+	</section>
+	<section class="aboutSection" id="about">
+		<h1 class="aSHeading">Who are<br/><span class="spn1">We?</span></h1>
+		<p class="aboutSubHeading">Wearing a MaazA watch enables entry into a world of unlimited possibilities.</p>
+		<div class="aboutPortion">
+			<div class="aboutLeft">
+				<img loading="lazy" src="img/aboutWatch.webp">
+			</div>
+			<div class="aboutRight">
+				<h1 class="arHeading">MaazA’s story began when founder Hans Wilsdorf created the first waterproof wristwatch – the Oyster – and developed into a range of timepieces that have become icons of watchmaking. Discover MaazA watches on the wrists of pioneers, artists, athletes and visionaries and journey to the very highest summits and the deepest part of the oceans.</h1>
+				<p>
+					An integrated and independent company, MaazA has manufacturing resources that encompass a wide range of skills and knowledge. Watchmakers, engineers, designers and other specialists work closely together from the design to the manufacturing of the watches. MaazA’s company culture is based above all on human values, which place both expertise and soft skills at the heart of the brand’s different activities.
+				</p>
+				<p>
+					MaazA is host to a variety of professions, brought to life by employees who are experts in their fields. This diversity comes from the company’s complete control over its essential components, from the casting of gold alloys to machining, finishing and assembly of the movement, case, dial and bracelet elements, as well as gem-setting. The brand is able to constantly reinforce its quality requirements thanks to its exclusive equipment. Its know-how is also made available right across the world thanks to an exceptional after-sales service network.
+				</p>
+				<button class="historyBtn">MaazA World</button>
+			</div>
+		</div>
+		<div class="waterproofnessSection">
+			<div class="wpHeader">
+				<h1>MaazA and waterproofness</h1>
+				<p>Mastering Waterproofness</p>
+				<button class="wpBtn">Read History</button>
+			</div>
+		</div>
+	</section>
+	<section class="contactSection" id="contact">
+			<div class="contactLeft">
+				<h1 class="contactHeading">Get<br/>Yourself<br/>a MaazA<br/>Watch</h1>
+			</div>
+			<div class="contactRight">
+				<form>
+					<label>Name</label>
+					<input type="text" placeholder="Name">
+					<label>Email Address</label>
+					<input type="email" placeholder="Email">
+					<label>Subject</label>
+					<input type="text" placeholder="Subject" class="subjectInput">
+					<label>Message</label>
+					<textarea rows="5" cols="40" placeholder="Message"></textarea>
+					<input type="submit" value="Send Message" class="submitBtn">
+				</form>
+			</div>
+	</section>
+	<footer>
+		<div class="footerLogo">
+			<img src="img/favicon.png"/>
+		</div>
+		<div class="footerLinks">
+			<div class="fLink1">
+				<ul>
+					<li><a href="#">Home</a></li>
+					<li><a href="#collection">Collection</a></li>
+					<li><a href="#about">About Us</a></li>
+					<li><a href="#contact">Contact</a></li>
+				</ul>
+			</div>
+			<div class="fLink0">
+				<h1>About Us</h1>
+				<p>An integrated and independent company, MaazA has manufacturing resources that encompass a wide range of skills and knowledge. Watchmakers, engineers, designers and other specialists work closely together from the design to the manufacturing of the watches. MaazA’s company culture is based above all on human values, which place both expertise and soft skills at the heart of the brand’s different activities.</p>
+			</div>
+			<div class="fLink2">
+				<h1>Headquaters</h1>
+				<p><i class="fas fa-map-marker-alt"></i> AGT, Rawalpindi, Pakistan</p>
+				<h1>Language</h1>
+				<p>English <i class="fas fa-angle-down"></i></p>
+			</div>
+			<div class="fLink3">
+				<h1>Subscribe to Our Newsletter</h1>
+				<form autocomplete="off">
+					<input type="text" placeholder="Name"/>
+					<input type="email" placeholder="Email"/>
+					<input type="submit" value="Subscribe">
+				</form>
+			</div>
+		</div>
+		<div class="socialIcons">
+			<a href="#"><i class="fa fa-facebook-square"></i></a>
+			<a href="#"><i class="fa fa-youtube-play"></i></a>
+			<a href="#"><i class="fa fa-instagram"></i></a>
+		</div>
+	</footer>
+</body>
+</html>
